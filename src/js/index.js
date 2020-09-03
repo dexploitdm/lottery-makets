c64e709743e2b0726f21272cd69e41c19d7af887
dexploitdm <knutdmit@yandex.ru>
2020-09-03 22:44:13 +0500

    init drawer

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,14 @@
 import $ from "jquery";
+
 import popper from "popper.js";
 import bootstrap from "bootstrap";
 import './module/form/select.js';
 
+global.IScroll = require('iscroll');
+import drawer from "jquery-drawer";
+$(document).ready(function() {
+    $('.drawer').drawer();
+});
 
 console.log($('#sources'))
 
