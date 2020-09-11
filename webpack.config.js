be5a642a674ffef5d8581426dbfe481902301964
Дмитрий Паршаков <d.parshakov@platiza.ru>
2020-09-11 13:30:01 +0500

    add module places

--- webpack.config.js
+++ webpack.config.js
@@ -110,6 +110,10 @@ const config = {
           from: "./src/uploads",
           to: "./uploads",
         },
+        {
+          from: "./src/js/components",
+          to: "./js/module",
+        },
       ],
     }),
   ].concat(htmlPlugins),
