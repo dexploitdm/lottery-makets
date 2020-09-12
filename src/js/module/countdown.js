c54f4a44626fb4b2f145ddccf7ecc1292b981d89
dexploitdm <knutdmit@yandex.ru>
2020-09-12 14:30:16 +0500

    fix timer

--- src/js/module/countdown.js
+++ src/js/module/countdown.js
@@ -1,28 +1,26 @@
 import $ from "jquery";
 
 $(document).ready(function() {
-    const second = 1000,
-        minute = second * 60,
-        hour = minute * 60,
-        day = hour * 24,
-        timeDate = $('.js-product-time').attr('data-countdown');
+    const timeDate = $('.js-product-time').attr('data-countdown');
+    if(timeDate.length >0){
+        const second = 1000,
+            minute = second * 60,
+            hour = minute * 60,
+            day = hour * 24;
 
-    let countDown = new Date(timeDate).getTime(),
-        x = setInterval(function() {
 
-            let now = new Date().getTime(),
-                distance = countDown - now;
+        let countDown = new Date(timeDate).getTime(),
+            x = setInterval(function() {
 
-            document.getElementById('days').innerText = Math.floor(distance / (day)),
-                document.getElementById('hours').innerText = Math.floor((distance % (day)) / (hour)),
-                document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute));
+                let now = new Date().getTime(),
+                    distance = countDown - now;
+
+                document.getElementById('days').innerText = Math.floor(distance / (day)),
+                    document.getElementById('hours').innerText = Math.floor((distance % (day)) / (hour)),
+                    document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute));
                 // document.getElementById('seconds').innerText = Math.floor((distance % (minute)) / second);
 
-            //do something later when date is reached
-            //if (distance < 0) {
-            //  clearInterval(x);
-            //  'IT'S MY BIRTHDAY!;
-            //}
+            }, second)
+    }
 
-        }, second)
 });
