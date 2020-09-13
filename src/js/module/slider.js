0df49daa9f964c7ce985e642badacdb32eaed779
dexploitdm <knutdmit@yandex.ru>
2020-09-13 15:37:14 +0500

    to begin page

--- src/js/module/slider.js
+++ src/js/module/slider.js
@@ -1,53 +1,86 @@
 import $ from "jquery";
 
-var stickersSlider = new Swiper('.stickers-slider', {
-    slidesPerView: 1,
-    spaceBetween: 55,
-    preventClicks: false,
-    preventClicksPropagation: false,
-    navigation: {
-        nextEl: '.next',
-        prevEl: '.prev'
-    },
-    scrollbar: {
-        el: '.slider-scrollbar',
-        draggable: true,
-        hide: false,
-        snapOnRelease: false,
-        dragSize: 30
-    },
-    breakpoints: {
-        1260: {
-            slidesPerView: 5
+$(document).ready(function() {
+    initSliderHome();
+    initSliderWins();
+})
+
+function initSliderHome() {
+
+    var stickersSlider = new Swiper('.stickers-slider', {
+        slidesPerView: 1,
+        spaceBetween: 55,
+        preventClicks: false,
+        preventClicksPropagation: false,
+        navigation: {
+            nextEl: '.next',
+            prevEl: '.prev'
         },
-        1050: {
-            slidesPerView: 4
+        scrollbar: {
+            el: '.slider-scrollbar',
+            draggable: true,
+            hide: false,
+            snapOnRelease: false,
+            dragSize: 30
         },
-        768: {
-            slidesPerView: 3
+        breakpoints: {
+            1260: {
+                slidesPerView: 5
+            },
+            1050: {
+                slidesPerView: 4
+            },
+            768: {
+                slidesPerView: 3
+            },
+            320: {
+                slidesPerView: 1
+            }
         },
-        320: {
-            slidesPerView: 1
+        on: {
+            init: function() {
+                $('.js-current-slide').text(this.realIndex + 1);
+                $('.js-all-slide').text(this.slides.length);
+            },
+            slideChange: function() {
+                $('.js-current-slide').text(this.realIndex + 1);
+            },
+            setTranslate: function() {
+                var progress = translateVal(this.scrollbar.dragEl);
+                $('.slider-progress2').css('width', progress + 'px');
+            },
+            slideChangeTransitionEnd: function() {
+                var progress = translateVal(this.scrollbar.dragEl);
+                $('.slider-progress2').css('width', progress + 'px');
+            }
         }
-    },
-    on: {
-        init: function() {
-            $('.js-current-slide').text(this.realIndex + 1);
-            $('.js-all-slide').text(this.slides.length);
+    });
+}
+
+function initSliderWins() {
+
+    var stickersSlider = new Swiper('.wins-slider-init', {
+        slidesPerView: 1,
+        spaceBetween: 55,
+        preventClicks: false,
+        preventClicksPropagation: false,
+        navigation: {
+            nextEl: '.next',
+            prevEl: '.prev'
         },
-        slideChange: function() {
-            $('.js-current-slide').text(this.realIndex + 1);
-        },
-        setTranslate: function() {
-            var progress = translateVal(this.scrollbar.dragEl);
-            $('.slider-progress2').css('width', progress + 'px');
-        },
-        slideChangeTransitionEnd: function() {
-            var progress = translateVal(this.scrollbar.dragEl);
-            $('.slider-progress2').css('width', progress + 'px');
+        breakpoints: {
+            1260: {
+                slidesPerView: 9
+            },
+            768: {
+                slidesPerView: 6
+            },
+            320: {
+                slidesPerView: 3
+            }
         }
-    }
-});
+    });
+}
 
 function translateVal(el) {
     var progress = el.style.transform.match(/translate3d\((.+)px,(.+)px,(.+)px\)/);
